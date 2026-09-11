6c0de4f121acffd12c680426abb71a362659e2df
Mitesh Kumar <miteshkumar82115@gmail.com>
2026-09-11 13:13:20 +0530

    Enhance datatype examples in 002_datatypes.js
    Added examples for null, undefined, strings, and symbols.

--- code/JS_Foundation/002_datatypes.js
+++ code/JS_Foundation/002_datatypes.js
@@ -46,3 +46,36 @@ const username = "miteshdotcom";
 
 console.log(username);
 
+
+//Null and Undefined
+
+let myVariable;
+console.log(myVariable); //undefined
+
+myVariable = null;
+console.log(myVariable); //null
+
+
+//String
+let myString="Hola";
+let myString1='Hola';
+let myString2=`Hola`;
+
+let combinedString=myString+myString1+myString2;
+console.log(combinedString);
+
+console.log(myString);
+console.log(myString1);
+console.log(myString2);
+
+//but the `` allows us to use variables inside the string
+let myName="Mitesh";
+let myString3=`Hello ${myName}`;
+console.log(myString3); //Hello Mitesh
+
+
+//Symbol
+let sym1=Symbol("Mitesh");
+let sym2=Symbol("Mitesh");
+console.log(sym1); //Symbol(Mitesh) 
+console.log(sym1==sym2); //false
