f7854d681299f229bd9e45ddd27e67ae34825892
Mitesh Kumar <miteshkumar82115@gmail.com>
2026-09-11 11:57:54 +0530

    Add files via upload

---
+++ code/JS_Foundation/001_hello.js
@@ -0,0 +1,15 @@
+console.log("Hello World!");
+process.stdout.write("Hello World!");
+process.stdout.write("India");
+console.log("country");
+
+console.table({ city: "Delhi" });
+console.warn("Dont GO THERE!!");
+
+//alert("HEY");
+
+let age = 21;
+
+// document.write("Age: " + age);
+console.info("HOLA");
+console.error("Bonjour")
---
+++ code/JS_Foundation/002_datatypes.js
@@ -0,0 +1,48 @@
+console.log("hello");
+// console.log("chai");
+/*
+
+String
+Number
+Boolean
+Bigint
+
+Undefined
+null
+
+Object
+
+Symbol
+
+*/
+
+// var score = 102
+
+let score = 102;
+let name = "chaicode.com";
+let isLoggedin = false;
+
+//object
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"];
+let user = { firstname: "hitesh", lastname: "choudhary" };
+
+let getScore = score;
+
+console.log(getScore);
+
+
+
+//Change value of variables
+
+let gameName = "spiderman";
+
+gameName = "batman";
+
+console.log(gameName);
+
+const username = "miteshdotcom";
+
+//username = "mitesh";  // this will cause error as assigning value to const variable
+
+console.log(username);
+
