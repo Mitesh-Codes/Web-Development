b16fcc1da57a47436304ebae912d986cd3072825
Mitesh Kumar <miteshkumar82115@gmail.com>
2026-09-16 16:50:58 +0530

    Add files via upload

---
+++ code/JS_Foundation/009_Prototypes.js
@@ -0,0 +1,22 @@
+let computer={cpu:12};
+let lenovo={screen:"HD",
+    __proto__:computer
+};
+let tomHardware={};
+
+console.log(lenovo.__proto__); // Object.prototype
+
+//or
+
+Object.setPrototypeOf(tomHardware, lenovo);
+console.log(tomHardware.screen)
+
+//or
+
+console.log(Object.getPrototypeOf(tomHardware));
+
+
+//What is Prototype?
+//Prototype is an object that is associated with every functions and objects by default in JavaScript. 
+//It allows you to add new properties and methods to existing objects constructors. 
+//It is a mechanism by which JavaScript objects inherit features from one another.
